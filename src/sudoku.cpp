a9c8242785792bec7289a34c63ac5a86accc7171
Otte Bokma <mutkaoja02@gmail.com>
2020-04-24 23:24:19 +0200

    input works

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <string>
 
+enum {
+    E_INVALIDINPUT = 1
+};
+
 using namespace std;
 
 int main()
 {
     string in_str;
     cin >> in_str;
+    // TO DO
+    //Ceck if input length is correct
 
-    int sudoku = stoi(in_str);
     int sudoku_arr[81];
 
-    
+    for (int i = 0; i != 81; ++i)
+    {
+        char const letter = in_str[i];
+        if (letter == '.')
+            sudoku_arr[i] = 0;
+        else if ('1' <= letter and letter <= '9')
+            sudoku_arr[i] = letter - '0';
+        else
+        {
+            cerr << "Invalid input: " << letter << '\n';
+            return E_INVALIDINPUT;
+        }
+    }
+    cout << '\n';
 
     //string sudoku_str = to_string(sudoku_arr);
 
-    cout << sudoku_arr << '\n';
+    //cout << sudoku_str << '\n';
 }
