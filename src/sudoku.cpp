64c81c3175ce83b9ddba047dc40da837d6d701dd
Otte Bokma <mutkaoja02@gmail.com>
2020-04-25 00:52:27 +0200

    commented

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -13,6 +13,7 @@ int main()
     string in_str;
     cin >> in_str;
 
+    //Checks if the input string length is correct
     if (in_str.length() != 81)
     {
         cerr << "Invalid input: wrong length\n";
@@ -20,32 +21,32 @@ int main()
     }
         
 
-    int sudoku_arr[81];
+    int sudoku_arr[81]; //Variable for array
 
     // Convert input string to array
     for (size_t pos = 0; pos != 81; ++pos)
     {
-        char const letter = in_str[pos];
-        if (letter == '.')
+        char const letter = in_str[pos];    //Makes input from position as char const
+        if (letter == '.')  //If the character i '.' puts 0 in array
             sudoku_arr[pos] = 0;
-        else if ('1' <= letter and letter <= '9')
+        else if ('1' <= letter and letter <= '9')   //If 1 <= character <= 9 puts that number in array
             sudoku_arr[pos] = letter - '0';
-        else
+        else //If none of the statements apply give error code for invalid inpus
         {
             cerr << "Invalid input: " << letter << '\n';
             return E_INVALIDINPUT;
         }
     }
 
-    string sudoku_str(81,'.');
+    string sudoku_str(81,'.'); //Variable for new string
 
     //Convert array to string
     for (size_t pos = 0; pos != 81; ++pos)
     {
-        int const num = sudoku_arr[pos];
-        if (1 <= num and num <= 9)
+        int const num = sudoku_arr[pos]; 
+        if (1 <= num and num <= 9)  //If 1 <= num <= 9 puts num in string as char
             sudoku_str[pos] = num + '0';
-        else if (num != 0)
+        else if (num != 0) //If num is not '.' or 1-9 give error code for invalid computation
         {
             cerr << "Invalid computation: number not 1-9 in array\n";
             return E_INVALIDCOMPUTATION;
@@ -53,10 +54,12 @@ int main()
         
     }
 
-    if (in_str == sudoku_str)
+    //Checks if input and output string match
+    if (in_str == sudoku_str) //If strings match print this
         cout << "Yayyy\n";
-    else
+    else //If strings do not match print  this
         cout << "ÄÄÄÄÄÄÄÄHHHHH NÖÖÖÖÖÖÖÖ\n";
+    
     cout << in_str << '\n';
     cout << sudoku_str << '\n';
 }
